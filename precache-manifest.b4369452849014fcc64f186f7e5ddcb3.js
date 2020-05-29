77fbc3f1dbac08bff4aa918f3d3cf7d0ac3a4c15
Passawit-Umrod <44994800+Passawit-Umrod@users.noreply.github.com>
2020-05-29 23:57:31 +0700

    Updates

---
+++ precache-manifest.b4369452849014fcc64f186f7e5ddcb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63bcaeb2d71bf2630a93c57c583162c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "394dde9da8bceaaf7f97",
+    "url": "/static/css/2.f1d904fc.chunk.css"
+  },
+  {
+    "revision": "019d7c8f6b2274f920bf",
+    "url": "/static/css/main.895e541c.chunk.css"
+  },
+  {
+    "revision": "394dde9da8bceaaf7f97",
+    "url": "/static/js/2.fd84fd1c.chunk.js"
+  },
+  {
+    "revision": "9c74705816f962d34fa0c0768ec423a0",
+    "url": "/static/js/2.fd84fd1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019d7c8f6b2274f920bf",
+    "url": "/static/js/main.5cd0dc42.chunk.js"
+  },
+  {
+    "revision": "d9ca1125059f274579d3",
+    "url": "/static/js/runtime-main.85649ae4.js"
+  }
+]);
