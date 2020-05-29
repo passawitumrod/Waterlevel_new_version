9a50c42c89faf8fe721c1e9817f63e38a1f1153e
Passawit-Umrod <44994800+Passawit-Umrod@users.noreply.github.com>
2020-05-29 23:41:05 +0700

    first commit

---
+++ src/components/ContentHome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+  export default function ContentHome() {
+    return (
+        <div className="content-wrapper" >
+            <b><p>Hello, This is the homepage and It will soon have cesium map window</p></b>
+            <b><p>This is the link to CESIUM map</p></b>
+            <b><p><a href="https://krittangcharoenkijkul.github.io/Cesium-Resium-WaterLevel/">https://krittangcharoenkijkul.github.io/Cesium-Resium-WaterLevel/</a></p></b>
+            <b><p>Other layout and formatting issues will be corrected</p></b>
+
+        </div>
+        
+        
+    )
+}
